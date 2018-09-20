1bb01714f9c288b09ac6d3e2ca681bb7dbe27a70
fennsaji <fennig1997@gmail.com>
2018-09-20 22:11:38 +0530

    Factory file to access deployed contract

---
+++ factory.js
@@ -0,0 +1,20 @@
+const Web3 = require('web3');  
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const BallotFactory =  require('./build/Ballot.json');
+const secrets = require('./secrets');
+
+const provider = new HDWalletProvider(
+    secrets.accountMnemonic,
+    secrets.rinkebyNode
+);
+
+let web3;
+web3 = new Web3(provider);
+
+
+const instance = new web3.eth.Contract(
+  JSON.parse(BallotFactory.interface),
+  secrets.deployedTo
+);
+
+module.exports =  {instance, web3};
