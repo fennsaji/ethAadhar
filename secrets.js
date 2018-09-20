9ebddcdc33383ba1d4c798b6c2e7bd73fba6487b
fennsaji <fennig1997@gmail.com>
2018-09-20 22:10:24 +0530

    Deployed code

---
+++ secrets.js
@@ -0,0 +1,5 @@
+module.exports =  { 
+    accountMnemonic: 'spice creek mule tennis wagon fly fruit spice anger absurd rival habit',
+    rinkebyNode :'https://rinkeby.infura.io/v3/5434edd6345b4302aacc6619c1673781',
+    deployedTo: '0x22d1C08bE47e402f92Fa4b054065073c41EDE41A'
+}
